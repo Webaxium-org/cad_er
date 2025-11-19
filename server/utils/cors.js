dda235109fb7d32663444995503ad992b16d8709
Muhammed Shafi SR <shafi.sd@webaxium.com>
2025-11-19 11:14:55 +0530

    multiple cors options

--- server/utils/cors.js
+++ server/utils/cors.js
@@ -1,8 +1,19 @@
 import cors from 'cors';
 
+const allowedOrigins = [process.env.CLIENT_URL, process.env.CLIENT_URL_2];
+
 const configureCors = () =>
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: (origin, callback) => {
+      // Allow requests with no origin (mobile apps, curl, postman)
+      // if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
   });
 
