32a81f1b946327f03faf15bfd6e2d51a1b6b56d2
Muhammed Shafi SR <shafi.sd@webaxium.com>
2025-12-02 20:01:28 +0530

    home page stack issue

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -1,27 +1,27 @@
-import { motion } from 'framer-motion';
-import { useEffect } from 'react';
-import { Stack, Box, Typography, Grid, Paper } from '@mui/material';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import { useDispatch, useSelector } from 'react-redux';
-import { stopLoading } from '../../redux/loadingSlice';
-import { useNavigate } from 'react-router-dom';
-import BasicInput from '../../components/BasicInput';
-import BasicCard from '../../components/BasicCard';
+import { motion } from "framer-motion";
+import { useEffect } from "react";
+import { Stack, Box, Typography, Grid, Paper } from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useDispatch, useSelector } from "react-redux";
+import { stopLoading } from "../../redux/loadingSlice";
+import { useNavigate } from "react-router-dom";
+import BasicInput from "../../components/BasicInput";
+import BasicCard from "../../components/BasicCard";
 
-import { IoNotificationsOutline } from 'react-icons/io5';
-import { MdOutlineSearch } from 'react-icons/md';
-import { TbReport } from 'react-icons/tb';
-import { TbReportSearch } from 'react-icons/tb';
+import { IoNotificationsOutline } from "react-icons/io5";
+import { MdOutlineSearch } from "react-icons/md";
+import { TbReport } from "react-icons/tb";
+import { TbReportSearch } from "react-icons/tb";
 
-import { TbRefresh } from 'react-icons/tb';
-import { MdGpsFixed } from 'react-icons/md';
-import { GoChecklist } from 'react-icons/go';
+import { TbRefresh } from "react-icons/tb";
+import { MdGpsFixed } from "react-icons/md";
+import { GoChecklist } from "react-icons/go";
 
-import BackgroundImage from '../../assets/background-img.png';
-import ImageAvatars from '../../components/ImageAvatar';
-import BasicButton from '../../components/BasicButton';
-import { IoIosAddCircleOutline } from 'react-icons/io';
-import BasicDivider from '../../components/BasicDevider';
+import BackgroundImage from "../../assets/background-img.png";
+import ImageAvatars from "../../components/ImageAvatar";
+import BasicButton from "../../components/BasicButton";
+import { IoIosAddCircleOutline } from "react-icons/io";
+import BasicDivider from "../../components/BasicDevider";
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -32,29 +32,29 @@ const Home = () => {
 
   const { global } = useSelector((state) => state.loading);
 
-  const above385 = useMediaQuery('(min-width:386px)');
-  const above400 = useMediaQuery('(min-width:400px)');
+  const above385 = useMediaQuery("(min-width:386px)");
+  const above400 = useMediaQuery("(min-width:400px)");
 
   const actions = [
     {
-      label: 'Projects',
+      label: "Projects",
       icon: <TbReport fontSize={above400 ? 32 : 28} />,
-      link: '/survey',
+      link: "/survey",
     },
     {
-      label: 'Reports',
+      label: "Reports",
       icon: <TbReportSearch fontSize={above400 ? 32 : 28} />,
-      link: '/survey/report',
+      link: "/survey/report",
     },
     {
-      label: 'Unit Con.',
+      label: "Unit Con.",
       icon: <TbRefresh fontSize={above400 ? 32 : 28} />,
-      link: '#',
+      link: "#",
     },
     {
-      label: 'Camera',
+      label: "Camera",
       icon: <MdGpsFixed size={28} />,
-      link: '/camera',
+      link: "/camera",
     },
   ];
 
@@ -63,60 +63,60 @@ const Home = () => {
   }, []);
 
   return (
-    <Stack spacing={2} pb={2} sx={{ userSelect: 'none' }}>
+    <Stack spacing={2} pb={2} sx={{ userSelect: "none" }}>
       {/* 🌈 HEADER */}
       <Stack
         p={2}
-        height={'228px'}
+        height={"228px"}
         sx={{
-          position: 'relative',
-          backgroundColor: 'rgba(40, 151, 255, 1)',
+          position: "relative",
+          backgroundColor: "rgba(40, 151, 255, 1)",
         }}
       >
         <div
           style={{
-            position: 'absolute',
+            position: "absolute",
             inset: 0,
             backgroundImage: `url(${BackgroundImage})`,
-            backgroundSize: '200%',
-            backgroundPosition: 'center',
+            backgroundSize: "200%",
+            backgroundPosition: "center",
             opacity: 0.25,
             zIndex: 0,
-            height: '60dvh',
-            width: '100%',
+            height: "60dvh",
+            width: "100%",
           }}
         ></div>
         <Box
-          display={'flex'}
-          justifyContent={'space-between'}
-          alignItems={'center'}
+          display={"flex"}
+          justifyContent={"space-between"}
+          alignItems={"center"}
           color="white"
         >
-          <Typography textAlign={'center'} fontWeight={900} fontSize="24px">
+          <Typography textAlign={"center"} fontWeight={900} fontSize="24px">
             CADer
           </Typography>
 
           <Box>
-            <IoNotificationsOutline fontSize={'24px'} />
+            <IoNotificationsOutline fontSize={"24px"} />
           </Box>
         </Box>
 
         <Stack
-          direction={'row'}
-          alignItems={'center'}
-          justifyContent={'space-between'}
+          direction={"row"}
+          alignItems={"center"}
+          justifyContent={"space-between"}
           mt={2}
         >
-          <Stack direction={'row'} alignItems={'center'} spacing={1}>
+          <Stack direction={"row"} alignItems={"center"} spacing={1}>
             <ImageAvatars
               sx={{
-                width: '48px',
-                height: '48px',
-                backgroundColor: '#fff',
-                color: 'rgba(40, 151, 255, 1)',
-                '& .css-1mo2pzk-MuiSvgIcon-root-MuiAvatar-fallback': {
-                  width: '60%',
-                  height: '60%',
+                width: "48px",
+                height: "48px",
+                backgroundColor: "#fff",
+                color: "rgba(40, 151, 255, 1)",
+                "& .css-1mo2pzk-MuiSvgIcon-root-MuiAvatar-fallback": {
+                  width: "60%",
+                  height: "60%",
                 },
               }}
             />
@@ -131,31 +131,31 @@ const Home = () => {
             </Box>
           </Stack>
 
-          <Box minWidth={'123px'}>
+          <Box minWidth={"123px"}>
             <BasicButton
               value={
                 <Box
-                  display={'flex'}
+                  display={"flex"}
                   gap={1}
-                  alignItems={'center'}
-                  color={'rgba(0, 111, 253, 1)'}
+                  alignItems={"center"}
+                  color={"rgba(0, 111, 253, 1)"}
                 >
                   <IoIosAddCircleOutline
-                    fontSize={'20px'}
-                    fontWeight={'900'}
-                    strokeWidth={'10px'}
+                    fontSize={"20px"}
+                    fontWeight={"900"}
+                    strokeWidth={"10px"}
                   />
-                  <Typography fontSize={'13px'} fontWeight={700}>
+                  <Typography fontSize={"13px"} fontWeight={700}>
                     Add Project
                   </Typography>
                 </Box>
               }
-              onClick={() => navigate('/survey/select-equipment')}
+              onClick={() => navigate("/survey/select-equipment")}
               sx={{
-                backgroundColor: '#ffffff',
-                height: '45px',
-                '&:hover': {
-                  backgroundColor: '#ffffff',
+                backgroundColor: "#ffffff",
+                height: "45px",
+                "&:hover": {
+                  backgroundColor: "#ffffff",
                 },
               }}
             />
@@ -164,20 +164,20 @@ const Home = () => {
 
         <BasicDivider borderBottomWidth={0.5} color="rgba(222, 222, 222, 1)" />
 
-        <Box position={'relative'}>
+        <Box position={"relative"}>
           <Box
-            position={'absolute'}
+            position={"absolute"}
             zIndex={1}
-            sx={{ top: '10px', left: '10px' }}
-            color={'rgba(145, 145, 145, 1)'}
+            sx={{ top: "10px", left: "10px" }}
+            color={"rgba(145, 145, 145, 1)"}
           >
-            <MdOutlineSearch fontSize={'24px'} />
+            <MdOutlineSearch fontSize={"24px"} />
           </Box>
           <BasicInput
             placeholder="Search"
             sx={{
-              paddingLeft: '40px',
-              borderRadius: '14px',
+              paddingLeft: "40px",
+              borderRadius: "14px",
             }}
           />
         </Box>
@@ -185,41 +185,41 @@ const Home = () => {
 
       <Box px={2} className="overlapping-header">
         {/* ⚡ Quick Actions */}
-        <Stack spacing={2} >
+        <Stack spacing={2}>
           <Typography fontWeight={700} fontSize="16px">
             Quick Actions
           </Typography>
-          <Box display={'flex'} justifyContent={'space-between'}>
+          <Box display={"flex"} justifyContent={"space-between"}>
             {actions.map((item, idx) => (
               <Box key={idx}>
-                <Stack alignItems={'center'} height={'100%'} spacing={0.5}>
+                <Stack alignItems={"center"} height={"100%"} spacing={0.5}>
                   <Paper
                     elevation={3}
                     sx={{
                       px: 2,
                       py: 1.5,
-                      borderRadius: '14px',
-                      textAlign: 'center',
-                      cursor: 'pointer',
-                      transition: 'all 0.3s ease',
-                      width: `${above400 ? '55px' : '46px'}`,
-                      height: '100%',
-                      backgroundColor: 'white',
-                      '&:hover': {
-                        transform: 'scale(1.05)',
+                      borderRadius: "14px",
+                      textAlign: "center",
+                      cursor: "pointer",
+                      transition: "all 0.3s ease",
+                      width: `${above400 ? "55px" : "46px"}`,
+                      height: "100%",
+                      backgroundColor: "white",
+                      "&:hover": {
+                        transform: "scale(1.05)",
                       },
                     }}
                     onClick={() => navigate(item.link)}
                   >
                     <Stack
-                      justifyContent={'center'}
-                      alignItems={'center'}
+                      justifyContent={"center"}
+                      alignItems={"center"}
                       spacing={0.5}
-                      sx={{ color: 'rgba(0, 111, 253, 1)' }}
+                      sx={{ color: "rgba(0, 111, 253, 1)" }}
                     >
                       {item.icon}
                       <Typography
-                        fontSize={above400 ? '12px' : '10px'}
+                        fontSize={above400 ? "12px" : "10px"}
                         fontWeight={700}
                         color="black"
                         align="center"
@@ -232,124 +232,130 @@ const Home = () => {
               </Box>
             ))}
           </Box>
+
+          {/* Overview */}
+          <Typography fontWeight={700} fontSize="16px">
+            Overview
+          </Typography>
+
+          <Grid container columns={12} spacing={2}>
+            <Grid size={{ xs: above385 ? 6 : 12 }}>
+              <BasicCard
+                key={global}
+                component={motion.div}
+                initial={{ opacity: 0, y: 15 }}
+                animate={{ opacity: 1, y: 0 }}
+                whileHover={{
+                  y: -5,
+                  boxShadow: "0px 8px 20px rgba(0,0,0,0.1)",
+                }}
+                transition={{ duration: 0.4, delay: 0.6 }}
+                sx={{
+                  borderBottom: "2px solid #006FFD",
+                  borderRadius: "16px",
+                  cursor: "pointer",
+                }}
+                content={
+                  <Stack spacing={1}>
+                    <Box
+                      display={"flex"}
+                      justifyContent={"space-between"}
+                      alignItems={"center"}
+                      gap={1.5}
+                    >
+                      <Typography fontWeight={600} fontSize="14px">
+                        Total Projects
+                      </Typography>
+
+                      <Box
+                        bgcolor={"rgba(234, 242, 255, 1)"}
+                        borderRadius={"6px"}
+                        padding={"8px 8px 6px"}
+                      >
+                        <TbReport
+                          size={20}
+                          style={{
+                            color: "rgba(0, 111, 253, 1)",
+                          }}
+                        />
+                      </Box>
+                    </Box>
+                    <Typography fontWeight={700} fontSize="24px">
+                      12
+                    </Typography>
+
+                    <Typography
+                      fontWeight={600}
+                      fontSize="14px"
+                      color="rgba(40, 151, 255, 1)"
+                    >
+                      Today +2
+                    </Typography>
+                  </Stack>
+                }
+              />
+            </Grid>
+            <Grid size={{ xs: above385 ? 6 : 12 }}>
+              <BasicCard
+                key={global}
+                component={motion.div}
+                initial={{ opacity: 0, y: 15 }}
+                animate={{ opacity: 1, y: 0 }}
+                whileHover={{
+                  y: -5,
+                  boxShadow: "0px 8px 20px rgba(0,0,0,0.1)",
+                }}
+                transition={{ duration: 0.4, delay: 0.8 }}
+                sx={{
+                  borderBottom: "2px solid rgba(57, 104, 58, 1)",
+                  borderRadius: "16px",
+                  cursor: "pointer",
+                  height: "100%",
+                }}
+                content={
+                  <Stack spacing={1}>
+                    <Box
+                      display={"flex"}
+                      justifyContent={"space-between"}
+                      alignItems={"center"}
+                      gap={1.5}
+                    >
+                      <Typography fontWeight={600} fontSize="14px">
+                        Total Tasks
+                      </Typography>
+
+                      <Box
+                        bgcolor={"rgba(57, 104, 58, 0.22)"}
+                        borderRadius={"6px"}
+                        padding={"8px 8px 6px"}
+                      >
+                        <GoChecklist
+                          size={20}
+                          style={{
+                            color: "rgba(57, 104, 58, 1)",
+                          }}
+                        />
+                      </Box>
+                    </Box>
+                    <Typography fontWeight={700} fontSize="24px">
+                      0
+                    </Typography>
+
+                    <Typography
+                      fontWeight={600}
+                      fontSize="14px"
+                      color="rgba(57, 104, 58, 1)"
+                    >
+                      Today 0
+                    </Typography>
+                  </Stack>
+                }
+              />
+            </Grid>
+          </Grid>
         </Stack>
 
-        {/* Overview */}
-        <Typography fontWeight={700} fontSize="16px" mb={1}>
-          Overview
-        </Typography>
-
-        <Grid container columns={12} spacing={2}>
-          <Grid size={{ xs: above385 ? 6 : 12 }}>
-            <BasicCard
-              key={global}
-              component={motion.div}
-              initial={{ opacity: 0, y: 15 }}
-              animate={{ opacity: 1, y: 0 }}
-              whileHover={{ y: -5, boxShadow: '0px 8px 20px rgba(0,0,0,0.1)' }}
-              transition={{ duration: 0.4, delay: 0.6 }}
-              sx={{
-                borderBottom: '2px solid #006FFD',
-                borderRadius: '16px',
-                cursor: 'pointer',
-              }}
-              content={
-                <Stack spacing={1}>
-                  <Box
-                    display={'flex'}
-                    justifyContent={'space-between'}
-                    alignItems={'center'}
-                    gap={1.5}
-                  >
-                    <Typography fontWeight={600} fontSize="14px">
-                      Total Projects
-                    </Typography>
-
-                    <Box
-                      bgcolor={'rgba(234, 242, 255, 1)'}
-                      borderRadius={'6px'}
-                      padding={'8px 8px 6px'}
-                    >
-                      <TbReport
-                        size={20}
-                        style={{
-                          color: 'rgba(0, 111, 253, 1)',
-                        }}
-                      />
-                    </Box>
-                  </Box>
-                  <Typography fontWeight={700} fontSize="24px">
-                    12
-                  </Typography>
-
-                  <Typography
-                    fontWeight={600}
-                    fontSize="14px"
-                    color="rgba(40, 151, 255, 1)"
-                  >
-                    Today +2
-                  </Typography>
-                </Stack>
-              }
-            />
-          </Grid>
-          <Grid size={{ xs: above385 ? 6 : 12 }}>
-            <BasicCard
-              key={global}
-              component={motion.div}
-              initial={{ opacity: 0, y: 15 }}
-              animate={{ opacity: 1, y: 0 }}
-              whileHover={{ y: -5, boxShadow: '0px 8px 20px rgba(0,0,0,0.1)' }}
-              transition={{ duration: 0.4, delay: 0.8 }}
-              sx={{
-                borderBottom: '2px solid rgba(57, 104, 58, 1)',
-                borderRadius: '16px',
-                cursor: 'pointer',
-                height: '100%',
-              }}
-              content={
-                <Stack spacing={1}>
-                  <Box
-                    display={'flex'}
-                    justifyContent={'space-between'}
-                    alignItems={'center'}
-                    gap={1.5}
-                  >
-                    <Typography fontWeight={600} fontSize="14px">
-                      Total Tasks
-                    </Typography>
-
-                    <Box
-                      bgcolor={'rgba(57, 104, 58, 0.22)'}
-                      borderRadius={'6px'}
-                      padding={'8px 8px 6px'}
-                    >
-                      <GoChecklist
-                        size={20}
-                        style={{
-                          color: 'rgba(57, 104, 58, 1)',
-                        }}
-                      />
-                    </Box>
-                  </Box>
-                  <Typography fontWeight={700} fontSize="24px">
-                    0
-                  </Typography>
-
-                  <Typography
-                    fontWeight={600}
-                    fontSize="14px"
-                    color="rgba(57, 104, 58, 1)"
-                  >
-                    Today 0
-                  </Typography>
-                </Stack>
-              }
-            />
-          </Grid>
-        </Grid>
-
-        <Stack spacing={2} mt={2} sx={{ display: 'none' }}>
+        <Stack spacing={2} mt={2} sx={{ display: "none" }}>
           <Typography fontWeight={700} fontSize="16px">
             Tasks
           </Typography>
